54e484b5478678d4c1fa3efe877dab78ea075bb1
Susanna Passos <susannapassos@hotmail.com>
2024-05-25 22:50:51 -0300

    Desafio de Projeto - Implementado

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Implementado
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Implementado
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"O app {nomeApp} foi instalado com sucesso em seu Nokia.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia (numero: "98799-7452", modelo: "Nokia C20", imei: "001987-05-010000-0", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone (numero: "99852-5153", modelo: "Iphone 14", imei: "001970-01-010000-0", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Uber");
